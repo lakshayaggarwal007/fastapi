376d30cfc128c2b5d8c13d386639f5223c4fd278
lakshay <lakshayaggarwal3@gmail.com>
2022-11-07 09:28:59 +0530

    databse

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
-import schemas 
+from fastapi import FastAPI,Depends
+import schemas , models 
+
+from database import engine,get_db
+from sqlalchemy.orm import Session
 app = FastAPI()
 
 
-
+models.Base.metadata.create_all(engine)
 
 
 
 @app.post('/blog')
-def create(blog: schemas.Blog):
+def create(blog: schemas.Blog ,db: Session = Depends(get_db) ):
     return blog
